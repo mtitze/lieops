9c7f6903151cd08398c8ba244d06771ed500649f
Malte Titze <mtitze@users.noreply.github.com>
2022-08-23 15:01:34 +0200

    t-parameter in tao

--- lieops/solver/tao.py
+++ lieops/solver/tao.py
@@ -7,14 +7,47 @@ from .common import getRealHamiltonFunction
 
 class integrator:
     
-    def __init__(self, hamiltonian, order=2, omega=1, delta=0.01, real=False, **kwargs):
+    def __init__(self, hamiltonian, order: int=2, omega=1, real=False, **kwargs):
         '''
         Class to manage Tao's l'th order symplectic integrator for non-separable Hamiltonians.
+        
+        Parameters
+        ----------
+        hamiltonian: poly
+            A poly class representing the Hamiltonian to be integrated.
+        
+        order: int, optional
+            The order of the integrator.
+        
+        omega: float, optional
+            The coupling constant between the two phase spaces.
+            
+        delta: float, optional
+            The step size of the integrator.
+            
+        real: boolean, optional
+            Whether or not the Hamiltonian should be treated as dependent on the (real) q/p coordinates
+            or (complex) xi/eta coordinates.
 
         Reference(s):
         [1]: Molei Tao: "Explicit symplectic approximation of nonseparable Hamiltonians: 
                          algorithm and long time performance", PhysRevE.94.043303 (2016).
         '''
+        self.set_hamiltonian(hamiltonian, real=real)
+
+        
+        
+        self.t = kwargs.get('t', 1)
+        self.omega = omega # the coupling between the two phase spaces
+        self.delta = kwargs.get('delta', omega**(-1/order)/100) # the underlying step size; the default value here comes from the fact that delta << omega**(-1/order) should hold (see Ref. [1]). We consider a factor of 100 for "<<" to hold.
+        self.set_order(order) # the order of the integrator (must be even)
+        self.make_error_estimations()
+        
+        # njet.poly keys required in dhamiltonian to obtain the gradient of the Hamiltonian for each Q and P index (starting from 0 to self.dim)
+        self._component1_keys = {w: tuple(0 if k != w else 1 for k in range(2*self.dim)) for w in range(self.dim)}
+        self._component2_keys = {w: tuple(0 if k != w + self.dim else 1 for k in range(2*self.dim)) for w in range(self.dim)}
+        
+    def set_hamiltonian(self, hamiltonian, real=False):
         self.dim = hamiltonian.dim
         if not real:
             hamiltonian = hamiltonian*-1j
@@ -23,15 +56,6 @@ class integrator:
             self.realHamiltonian = getRealHamiltonFunction(hamiltonian, **kwargs)
             self.dhamiltonian = derive(self.realHamiltonian, order=1, n_args=self.dim*2)
         self.hamiltonian = hamiltonian
-
-        self.delta = delta # the underlying step size
-        self.omega = omega # the coupling between the two phase spaces
-        self.set_order(order) # the order of the integrator (must be even)
-        self.make_error_estimations() # perform some error estimations regarding the input parameters
-        
-        # njet.poly keys required in dhamiltonian to obtain the gradient of the Hamiltonian for each Q and P index (starting from 0 to self.dim)
-        self._component1_keys = {w: tuple(0 if k != w else 1 for k in range(2*self.dim)) for w in range(self.dim)}
-        self._component2_keys = {w: tuple(0 if k != w + self.dim else 1 for k in range(2*self.dim)) for w in range(self.dim)}
             
     def set_order(self, order):
         '''
@@ -40,15 +64,15 @@ class integrator:
         # TODO: may use other (improved) schemes...
         assert order%2 == 0, 'Order has to be even.'
         self.order = order
-        scheme = {2: [self.delta]}
+        scheme = {2: [1]}
         for l in range(4, order + 1, 2):
             gamma_l = 1/(2 - 2**(1/(l + 1)))
-            f1 = [delta*gamma_l for delta in scheme[l - 2]]
-            f2 = [delta*(1 - 2*gamma_l) for delta in scheme[l - 2]]
+            f1 = [f*gamma_l for f in scheme[l - 2]]
+            f2 = [f*(1 - 2*gamma_l) for f in scheme[l - 2]]
             scheme[l] = f1 + f2 + f1
         self.scheme = scheme[order]
-        self.cos_sin = [[np.cos(2*self.omega*delta), np.sin(2*self.omega*delta)] for delta in self.scheme]
-        
+        self.cos_sin = [[np.cos(2*self.omega*f*self.delta), np.sin(2*self.omega*f*self.delta)] for f in self.scheme]
+
     def make_error_estimations(self, show=False, warn=True):
         '''
         Perform some checks to help deciding whether omega, delta and the order
@@ -66,12 +90,17 @@ class integrator:
         '''
         l = self.order
         error_estimations = {}
+        error_estimations['order'] = self.order
+        error_estimations['delta'] = self.delta
+        error_estimations['omega'] = self.omega
         error_estimations['error'] = self.delta**l*self.omega # the global error of the solution towards the exact result (for integrable systems)
         error_estimations['qx_py'] = 1/np.sqrt(self.omega) # the error q - x, resp. p - y.
         error_estimations['tmax'] = min([self.delta**(-l)/self.omega, np.sqrt(self.omega)])
         error_estimations['delta_vs_omega'] = [self.delta, self.omega**(-1/l)]
         if self.delta*10 > error_estimations['delta_vs_omega'][1] and warn:
             warnings.warn(f"It appears that {self.delta} = delta << omega**(-1/order) = {error_estimations['delta_vs_omega'][1]} is not satisfied.")
+        if self.t > error_estimations['tmax']:
+            warnings.warn(f"Requested end time {self.t} > {error_estimations['tmax']} ~ T_max.")
             
         self.error_estimations = error_estimations
         if show:
@@ -80,19 +109,20 @@ class integrator:
             print (f'order: {l}')
             print (f'omega: {self.omega}')
             print (f'delta: {self.delta}')
+            print (f' time: {self.t}')
             print ('\nError estimates')
             print ('---------------')
-            print (f"    Against exact solution: O({error_estimations['error']}*t)")
-            print (f"          Up to (t=) T_max: O({error_estimations['tmax']})")
+            print (f"    Against exact solution: O({error_estimations['error']}*t) = O({error_estimations['error']*self.t})")
+            print (f"          up to (t=) T_max: O({error_estimations['tmax']})")
             print (f"delta << omega**(-1/order): {error_estimations['delta_vs_omega']} (?)")
             # print (f'         |q - x|, |p - y| ~ O({self.err_qx_py})')
-                        
-    def second_order_map(self, *qp, delta, w):
+            
+    def second_order_map(self, *qp, delta, cs):
         q, p = list(qp[:self.dim]), list(qp[self.dim:])
         z0 = [q, p, q, p]
         z1 = self.phi_HA(*z0, delta=delta/2)
         z2 = self.phi_HB(*z1, delta=delta/2)
-        z3 = self.phi_HC(*z2, w)
+        z3 = self.phi_HC(*z2, cs)
         z4 = self.phi_HB(*z3, delta=delta/2)
         z5 = self.phi_HA(*z4, delta=delta/2)
         return list(itertools.chain.from_iterable(z5[:2*self.dim])) # the elements of z5 are lists, we concatenate them together to have consistent output; see https://stackoverflow.com/questions/716477/join-list-of-lists-in-python
@@ -113,8 +143,8 @@ class integrator:
             result4.append(y[k] - dham.get(self._component1_keys[k], 0)*delta)
         return result1, p, x, result4
     
-    def phi_HC(self, q, p, x, y, w):
-        cos, sin = self.cos_sin[w]
+    def phi_HC(self, q, p, x, y, cs):
+        cos, sin = cs
         result1, result2, result3, result4 = [], [], [], []
         for k in range(self.dim):
             diff1 = q[k] - x[k]
@@ -129,12 +159,39 @@ class integrator:
             result4.append((sum2 - r2)*0.5)
         return result1, result2, result3, result4
     
-    def __call__(self, *xieta0):
+    def step(self, *xieta0, **kwargs):
+        '''
+        Integrate the equations of motion using a single delta-step of the integrator.
+        '''
         n = len(self.scheme)
+        if 'delta' in kwargs.keys():
+            delta = kwargs['delta']
+            cs = [[np.cos(2*self.omega*f*delta), np.sin(2*self.omega*f*delta)] for f in self.scheme]
+        else:
+            delta = self.delta
+            cs = self.cos_sin
+
+        cs = kwargs.get('cs', self.cos_sin)
         xieta = xieta0
         # TODO: combine adjacent phi_HA-maps
         for w in range(n):
-            delta = self.scheme[n - 1 - w]
-            xieta = self.second_order_map(*xieta, delta=delta, w=w)
+            xieta = self.second_order_map(*xieta, delta=self.scheme[n - 1 - w]*delta, cs=cs[n - 1 - w])
         return xieta
+    
+    def __call__(self, *xieta0, trajectory=False):
+        n_reps, r = divmod(self.t, self.delta)
+        xieta_all = []
+        xieta = xieta0
+        for k in range(int(n_reps)):
+            xieta = self.step(*xieta)
+            xieta_all.append(xieta)
+        if r > 0:
+            xieta = self.step(*xieta, delta=r*self.delta)
+            xieta_all.append(xieta)
+            
+        if trajectory:
+            return xieta_all
+        else:
+            return xieta
+        
         
